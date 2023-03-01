74d86c2491f5bd1178823ae3217bb6203b241a22
imtiyazgithub <imtiyazshirsangi456@gmail.com>
2023-03-01 09:29:56 +0530

    first commit

---
+++ task1.js
@@ -0,0 +1,32 @@
+//the execution context work for the following javascript code.
+
+
+
+// Question 1 
+
+
+
+var number1 = 2;
+
+var number2 = 5
+
+
+
+function add(a, b){
+
+var answer = a + b; //line 1
+
+return answer;
+
+}
+
+var sum = add(number1, number2); // line 2
+
+
+
+console.log(sum) //line 3
+
+
+
+console.log(answer) //line 4
+
