60fef8b436d2ed99490b3bc6b10b55ff4f77b91a
imtiyazgithub <imtiyazshirsangi456@gmail.com>
2023-03-01 09:42:51 +0530

    first commit

---
+++ task3.js
@@ -0,0 +1,43 @@
+//let ,var Const
+
+//ReferenceError:Raised when an invalid reference is used.
+ 
+//here we need to put a valid input otherwise it gives an reference error
+
+//SyntaxError: Raised when a syntax error occurs while parsing JavaScript code.
+
+
+//TypeError: Raised when the type of a variable is not as expected.
+
+
+//QUESTION 1
+
+
+
+console.log(a)
+
+console.log(b);
+
+//let a =5;
+
+var b =6;
+
+console.log(b);
+
+// QUESTION 2
+
+
+
+let a = 5;
+
+var b = 6;
+
+
+
+console.log(this.b);
+
+console.log(window.b)
+
+console.log(window.a)
+
+console.log(this.a);
