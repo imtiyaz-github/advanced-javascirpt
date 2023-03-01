718c71c624b424a91e2325a30f7cc7e58667c245
imtiyazgithub <imtiyazshirsangi456@gmail.com>
2023-03-01 11:02:38 +0530

    first commit

---
+++ task5.js
@@ -0,0 +1,105 @@
+
+
+// var a = 50;
+
+// {
+//     var a =30;
+//     let b = 20;
+//     let c =30;
+// }
+// console.log(a);
+
+//output 30
+
+
+// let  a =50
+// {
+//     var a =30;
+//     let b =20;
+//     let c =30;
+// }
+// console.log(a);
+
+//output error a is already declared
+
+
+
+// var a  =50;
+
+//  function fun() {
+//      var a =30;
+//     let b = 20;
+//     let c= 30;
+
+// }
+
+// fun();
+// console.log(a);
+
+//output 50;
+
+
+// let a =50;
+
+// function fun() {
+//     var a =30;
+//     let b =20;
+//     let c =30;
+// }
+// fun();
+// console.log(a);
+
+// const a =10;
+// {
+//     var a =100;
+
+// }
+// console.log(a);
+
+//output error var can come out of block scope
+
+
+
+// const a = 10;
+
+// {
+
+// const a = 20;
+
+// {
+
+// const a = 50;
+
+// console.log(a);
+
+// }
+
+// console.log(a)
+
+// }
+
+// console.log(a)
+//output 50 20 10
+
+
+// const a = 10;
+
+// {
+
+// const a = 20;
+
+// {
+
+// console.log(a);
+
+// }
+
+// console.log(a)
+
+// }
+
+// console.log(a)
+
+//output 20 20 10
+
+
