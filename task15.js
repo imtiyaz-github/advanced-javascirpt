d73a117aeabf2a40c8f6081f3140fc7dadb5925a
imtiyazgithub <imtiyazshirsangi456@gmail.com>
2023-03-04 21:07:00 +0530

    first commit

---
+++ task15.js
@@ -0,0 +1,97 @@
+
+//Event Loop
+
+//que 1
+
+
+// function fun1() {
+//     console.log(a)
+// }
+// function fun2() {
+//     console.log(b)
+// }
+
+// fun2();
+// fun1();
+
+//output a and b is not defined
+
+
+//que 2
+// function fun1() {
+//     console.log(a)
+// }
+
+// function fun2() {
+//     console.log(b)
+// }
+// fun2();
+// fun1();
+
+//que 3
+
+// function fun1() {
+//     console.log("a")
+// }
+// function fun2() {
+//     console.log(b)
+// }
+// setTimeout(fun2,1000)
+// fun1()
+
+//output a and wait 1 sec  b is not defined
+
+
+//que 4
+
+// function fun1() {
+//     console.log('a')
+// }
+// function fun2() {
+//     console.log(b)
+// }
+
+
+//que 6
+
+// function fun1() {
+//     console.log('a')
+// }
+// function fu2() {
+//     console.log(b)
+// }
+
+// setTimeout(fun2,1000)
+// fun1()
+
+//b is not defined
+
+
+//que 7
+
+// function fun1() {
+//     console.log("a")
+
+// }
+// function fun2() {
+//     console.log(b)
+// }
+// document.getElementById('btn')
+// .addEventListener('click',fun1)
+// fun2();
+
+
+
+
+
+
+
+
+// console.log("start");
+
+// document.getElementById('btn')
+// .addEventListener('click',function cb() {
+//     console.log('Callback');
+// });
+
+// console.log("End");
